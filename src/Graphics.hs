22d5c69e8ba25c576baf0dce1679306b386294a2
Samuel Gélineau <gelisam+github@gmail.com>
2014-12-05 22:24:43 -0500

    draw a grid

---
+++ src/Graphics.hs
@@ -0,0 +1,33 @@
+module Graphics where
+
+import Control.Applicative
+import Data.Monoid
+import Graphics.Gloss
+
+import Vec2d
+import Debug.Trace
+
+
+pictureRow :: Float -> [Picture] -> Picture
+pictureRow dx [] = blank
+pictureRow dx (x:xs) = x <> translate dx 0 (pictureRow dx xs)
+
+pictureCol :: Float -> [Picture] -> Picture
+pictureCol dy [] = blank
+pictureCol dy (x:xs) = x <> translate 0 dy (pictureCol dy xs)
+
+
+grid :: Float -> Float -> [[Picture]] -> Picture
+grid _ _ [] = blank
+grid cellSizeX cellSizeY cells = traceShow (cellSizeV, dimV, totalSizeV, centerX, centerY) $ translate (-centerX) (-centerY)
+                               $ pictureCol cellSizeY
+                               $ pictureRow cellSizeX
+                             <$> cells
+  where
+    w = length (head cells)
+    h = length cells
+    dimV = V (fromIntegral w) (fromIntegral h)
+    
+    cellSizeV = V cellSizeX cellSizeY
+    totalSizeV = cellSizeV * dimV
+    V centerX centerY = totalSizeV / 2
--- src/Main.hs
+++ src/Main.hs
@@ -5,14 +5,18 @@ import Graphics.Gloss.Interface.FRP.ReactiveBanana
 import Reactive.Banana
 import Reactive.Banana.Frameworks
 
+import Graphics
 import Vec2d
 
+
+emptyStage :: [[Picture]]
+emptyStage = replicate 10 (replicate 10 (circle 10))
 
 mainBanana :: Frameworks t
            => Event t Float
            -> Event t InputEvent
            -> Moment t (Behavior t Picture)
-mainBanana _ _ = return (pure (circle 10))
+mainBanana _ _ = return (pure (grid 20 20 emptyStage))
 
 main :: IO ()
 main = playBanana (InWindow "Ludum Dare 31" (640, 480) (800, 50))
