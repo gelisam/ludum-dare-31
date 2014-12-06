59c459876170bd27f3ba3695b48e51da1354a9d3
Samuel Gélineau <gelisam+github@gmail.com>
2014-12-05 20:59:33 -0500

    an empty window

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,18 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.FRP.ReactiveBanana
+import Reactive.Banana
+import Reactive.Banana.Frameworks
 
+
+mainBanana :: Frameworks t
+           => Event t Float
+           -> Event t InputEvent
+           -> Moment t (Behavior t Picture)
+mainBanana _ _ = return (pure (circle 10))
 
 main :: IO ()
-main = putStrLn "typechecks."
+main = playBanana (InWindow "Ludum Dare 31" (640, 480) (800, 50))
+                  white
+                  60
+                  mainBanana
